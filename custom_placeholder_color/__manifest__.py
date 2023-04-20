72ed0579e738e994edc50e7f75b6225cd5a22ffa
satish987 <msatisha92@gmail.com>
2023-04-20 21:30:49 +0530

    cover image

--- custom_placeholder_color/__manifest__.py
+++ custom_placeholder_color/__manifest__.py
@@ -1,40 +1,40 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Custom Placeholder Color",
+    "name": "Custom Placeholder Color",
 
-    'summary': """
+    "summary": """
         This Module Allows you to Placeholder Font Color Brighter in Odoo v16.""",
 
-    'description': """
+    "description": """
         This Module Allows you to Placeholder Font Color Brighter in Odoo v16.
     """,
 
-    'author': "Ceres IT Solution",
-    'website': "http://www.ceresitsolution.com",
+    "author": "Ceres IT Solution",
+    "website": "http://www.ceresitsolution.com",
     "license": "AGPL-3",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Extra Rights',
-    'version': '0.1',
+    "category": "Extra Rights",
+    "version": "0.1",
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
+    "depends": ["base"],
+    "images":["static/description/placeholder.png"],
     # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "data": [
+        # "security/ir.model.access.csv",
+        "views/views.xml",
+        "views/templates.xml",
     ],
-    'assets': {
-        'web.assets_backend': [
-            'custom_placeholder_color/static/src/css/main.scss',
+    "assets": {
+        "web.assets_backend": [
+            "custom_placeholder_color/static/src/css/main.scss",
 
         ], },
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
 }
